25bb7856c68a16640e99d019ee5b3f3a77bdb78f
Salma <saloumia18@gmail.com>
2023-04-22 13:04:19 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Determines if a number is positive, negative or zero.
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -27,3 +27,4 @@ int main(void)
 	}
 	return (0);
 }
+
